61c1168f3d03f89f7fdf9e5aa635f803bd7c880c
luunaandrade <luunaandrade@gmail.com>
2025-04-10 10:02:11 -0300

    Projeto inicial da galeria com arquivos estáticos

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('galeria/', views.galeria, name='galeria'),
 ] 
--- galeria/views.py
+++ galeria/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/home.html')
+
+def galeria(request):
+    return render(request, 'galeria/galeria.html')
